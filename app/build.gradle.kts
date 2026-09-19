225aeca719d981cc02d5937bec3458641176a928
rudra <rudraptl11903@gmail.com>
2026-09-19 18:12:00 +0530

    ..

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,17 +3,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mad_assignment"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    namespace = "com.example.aitimetable"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.mad_assignment"
+        applicationId = "com.example.aitimetable"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -22,24 +18,23 @@ android {
 
     buildTypes {
         release {
-            optimization {
-                enable = false
-            }
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
 dependencies {
-    implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/example/mad_assignment/Subject.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mad_assignment
-
-data class Subject(
-    val name: String,
-    val day: String,
-    val startTime: String,
-    val endTime: String,
-    val room: String
-)
